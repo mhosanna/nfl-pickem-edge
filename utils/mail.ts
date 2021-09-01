01be8f2425a81567a943ab441ec76a35a32de0a0
Madeline Dale <mdale@ebsco.com>
2021-09-01 10:50:13 -0400

    prod email transport

--- utils/mail.ts
+++ utils/mail.ts
@@ -1,5 +1,7 @@
 import { createTransport, getTestMessageUrl } from "nodemailer";
+import postmarkTransport from "nodemailer-postmark-transport";
 
+//dev
 const transport = createTransport({
   host: process.env.MAIL_HOST,
   port: Number(process.env.EMAIL_PORT) || 0,
@@ -8,6 +10,15 @@ const transport = createTransport({
     pass: process.env.MAIL_PASS,
   },
 });
+
+//prod
+const mailTransport = createTransport(
+  postmarkTransport({
+    auth: {
+      apiKey: process.env.POSTMARK_API,
+    },
+  })
+);
 
 function makeANiceEmail(text: string) {
   return `
@@ -26,16 +37,32 @@ function makeANiceEmail(text: string) {
 }
 
 export async function sendPasswordResetEmail(resetToken: string, to: string) {
-  // email the user a token
-  const info = await transport.sendMail({
+  const email = {
     to,
     from: "madeline@pick-em.club",
     subject: "Your password reset token!",
     html: makeANiceEmail(`Your Password Reset Token is here!
         <a href="${process.env.FRONTEND_URL}/reset?token=${resetToken}">Click Here to reset</a>
       `),
-  });
+  };
+  // email the user a token
   if (process.env.MAIL_USER.includes("ethereal.email")) {
-    console.log(`💌 Message Sent!  Preview it at ${getTestMessageUrl(info)}`);
+    const info = await transport
+      .sendMail(email)
+      .then(() =>
+        console.log(
+          `💌 Message Sent!  Preview it at ${getTestMessageUrl(info)}`
+        )
+      )
+      .catch((error) =>
+        console.error("There was an error while sending the email:", error)
+      );
+  } else {
+    await mailTransport
+      .sendMail(email)
+      .then(() => console.log("💌 Email sent successfully!"))
+      .catch((error) =>
+        console.error("There was an error while sending the email:", error)
+      );
   }
 }
